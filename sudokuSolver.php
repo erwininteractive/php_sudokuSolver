ae138ac1a2baa77eb79351c7b316db394a6b362a
Andrew S Erwin <andrew@erwininteractive.com>
2023-01-31 14:23:56 -0600

    brackets

--- sudokuSolver.php
+++ sudokuSolver.php
@@ -8,15 +8,15 @@
  */
 
 $puzzle = [
-  [0, 0, 0, 1, 0, 2, 0, 0, 0],
-  [0, 6, 0, 0, 0, 0, 0, 7, 0],
-  [0, 0, 8, 0, 0, 0, 9, 0, 0],
-  [4, 0, 0, 0, 0, 0, 0, 0, 3],
-  [0, 5, 0, 0, 0, 7, 0, 0, 0],
-  [2, 0, 0, 0, 8, 0, 0, 0, 1],
-  [0, 0, 9, 0, 0, 0, 8, 0, 5],
-  [0, 7, 0, 0, 0, 0, 0, 6, 0],
-  [0, 0, 0, 3, 0, 4, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 $s = new Sudoku($puzzle);
@@ -24,74 +24,71 @@ print $s->arrayToMatrix();
 
 class Sudoku
 {
-  protected $sudoku = [];
+    protected $sudoku = [];
 
-  public function __construct(array $puzzle)
-  {
-    $this->sudoku = $puzzle;
-    $this->solve();
-  }
-
-  private function isPossible(int $x, int $y, int $n): bool
-  {
-    foreach (range(0, 8) as $i) {
-      if ($this->sudoku[$y][$i] == $n) {
-        return false;
-      }
+    public function __construct(array $puzzle) {
+        $this->sudoku = $puzzle;
+        $this->solve();
     }
 
-    foreach (range(0, 8) as $i) {
-      if ($this->sudoku[$i][$x] == $n) {
-        return false;
-      }
-    }
-
-    $x0 = (floor($x / 3) * 3);
-    $y0 = (floor($y / 3) * 3);
-
-    foreach (range(0, 2) as $i) {
-      foreach (range(0, 2) as $j) {
-        if ($this->sudoku[$y0 + $i][$x0 + $j] == $n) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
-
-  private function solve(): bool
-  {
-    foreach (range(0, 8) as $y) {
-      foreach (range(0, 8) as $x) {
-        if ($this->sudoku[$y][$x] == 0) {
-          foreach (range(1, 9) as $n) {
-            if ($this->isPossible($x, $y, $n)) {
-              $this->sudoku[$y][$x] = $n;
-
-              if ($this->solve()) {
-                return true;
-              }
-
-              $this->sudoku[$y][$x] = 0;
+    private function isPossible(int $x, int $y, int $n): bool {
+        foreach (range(0, 8) as $i) {
+            if ($this->sudoku[$y][$i] == $n) {
+                return false;
             }
-          }
-
-          return false;
         }
-      }
-    }
-    return true;
-  }
 
-  public function arrayToMatrix()
-  {
-    foreach ($this->sudoku as $row) {
-      foreach ($row as $node) {
-        print $node . " ";
-      }
+        foreach (range(0, 8) as $i) {
+            if ($this->sudoku[$i][$x] == $n) {
+                return false;
+            }
+        }
 
-      print PHP_EOL;
+        $x0 = (floor($x / 3) * 3);
+        $y0 = (floor($y / 3) * 3);
+
+        foreach (range(0, 2) as $i) {
+            foreach (range(0, 2) as $j) {
+                if ($this->sudoku[$y0 + $i][$x0 + $j] == $n) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
-  }
+
+    private function solve(): bool {
+        foreach (range(0, 8) as $y) {
+            foreach (range(0, 8) as $x) {
+                if ($this->sudoku[$y][$x] == 0) {
+                    foreach (range(1, 9) as $n) {
+                        if ($this->isPossible($x, $y, $n)) {
+                            $this->sudoku[$y][$x] = $n;
+
+                            if ($this->solve()) {
+                                return true;
+                            }
+
+                            $this->sudoku[$y][$x] = 0;
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function arrayToMatrix(): void {
+        foreach ($this->sudoku as $row) {
+            foreach ($row as $node) {
+                print $node . " ";
+            }
+
+            print PHP_EOL;
+        }
+    }
 }
